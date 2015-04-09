1f35b5fc2c8d378afbf4bf268b2232b1c4971c36
Abeer Khakwani & tommy bountham <student@epicodus.com>
2015-04-09 10:29:46 -0700

    Add initial files and spec

---
+++ spec/specs.js
@@ -0,0 +1,14 @@
+describe('wordOrder', function() {
+    it("returns in order how many times a word came up", function() {
+        expect(Scrabble("a")).to.equal(1);
+    });
+
+
+
+
+
+
+
+
+
+});
