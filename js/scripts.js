a51bbb4ded16f952994d3ad39ee73d04e004c333
Abeer Khakwani & tommy bountham <student@epicodus.com>
2015-04-09 14:55:23 -0700

    for each loop word

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
+// var wordCount = function(input) {
+//     var inputArray = input.split(" ");
+//     var arrayChecking = {};
+//
+//
+//     inputArray.forEach(function(word, index) {
+//
+//         if (arrayChecking[word]) {
+//
+//             arrayChecking[word] += 1;
+//         } else {
+//             arrayChecking[word] = 1;
+//
+//         }
+//
+//
+//     });
+//
+//
+//     return arrayChecking;
+//
+//
+// }
+//
+//
 var wordCount = function(input) {
     var inputArray = input.split(" ");
-    var arrayChecking = [];
-
-
+    var arrayChecking = {};
+    var sorted=[];
+    var returnString= "";
     inputArray.forEach(function(word, index) {
-
 
         if (arrayChecking[word]) {
 
@@ -14,8 +38,19 @@ var wordCount = function(input) {
 
         }
     });
-    
-    return arrayChecking;
+    for(var word in arrayChecking){
+        sorted.push([word, arrayChecking[word]]);
+        sorted.sort(function(a,b){return b[1]-a[1]});
+    }
+    //debugger;
+    for(var i = 0; sorted.length > i; i++){
+        returnString+=" " +sorted[i][0];
+
+    }
+    return returnString;
+
+
+
 
 
 }
--- spec/specs.js
+++ spec/specs.js
@@ -9,6 +9,6 @@ describe('wordOrder', function() {
     //     expect(wordCount("a a b b c d")).to.eql({a:2, b:2 ,c:1 , d:1});
     // });
     it("if one word is reapeat more then another word it comes up first", function() {
-        expect(wordCount("b a a b a c")).to.eql({c:1, a:3, b:2});
+        expect(wordCount("b b a b a c")).to.eql({b:3,a:2,c:1, });
     });
 });
